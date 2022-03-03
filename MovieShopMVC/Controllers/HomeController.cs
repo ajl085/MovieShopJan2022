af74666d6c18634a4eb1084c596a24cc9ca4f3a2
ajl085 <ajl085@ucsd.edu>
2022-03-03 05:17:50 -0800

    Add project files.

---
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ApplicationCore.Contracts.Services;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+using System.Diagnostics;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMovieService _movieService;
+        public HomeController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // var movieService = new MovieService();
+            // model data
+            var movies = _movieService.GetTop30GrossingMovies();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopMovies()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
